177331d3a24b78ce839a4a5487f7f8796b87cd1f
SaiAdithya3 <saiadithyakancharla@gmail.com>
2024-04-20 19:12:00 +0530

    smtp pass chabge

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ import jwt from 'jsonwebtoken';
 
 const app = express();
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://re-event-backend.onrender.com','https://rvent.vercel.app'],
+    origin: ['http://localhost:5173', 'https://re-event-backend.onrender.com','https://rvent.vercel.app'],
     credentials: true,
 }));
 app.use(express.json());
